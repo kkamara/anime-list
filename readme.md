3d0ceac11bc93219651112359fb696fe6db1afd4
kkamara <kamaracomputers@gmail.com>
2024-10-06 21:54:01 +0100

    Add project

--- readme.md
+++ readme.md
@@ -1,8 +1,21 @@
 <img src="https://github.com/kkamara/useful/raw/main/MainClass.png" alt="MainClass.png" width=""/>
 
-# PhP Docker Skeleton
+# Anime List
 
-(2020) Get started with php and docker.
+(06-Oct-2024) This is a list of top-rated anime. I have watched them all. The order doesn't matter.
+
+- Akira (1988) (dubbed)
+- Bersek Movies (2012 - 2013) (dubbed)
+- Berserk (1997) (dubbed)
+- Claymore (2007) (dubbed)
+- Death Note (2006) (dubbed)
+- Death Note Light Up The New World (2016) (only subtitles, no dubbed exists)
+- Death Note: The Last Name (2006) (dubbed)
+- Highschool of the dead (2010) (dubbed)
+- Monster (2004) (dubbed)
+- Naruto (2002) (dubbed)
+- Phantom: Requiem for the Phantom (2009) (dubbed)
+- Pokémon: The First Movie Mewtwo Strikes Back (2000).
 
 ## Installation
 
@@ -36,8 +49,11 @@ make stop  # stop your docker container
 .\ps\run.ps1 stop  # stop your docker container
 ```
 
-## Want to use Python instead? 
-[Python Docker Skeleton](https://github.com/kkamara/python-docker-skeleton)
+## Misc
+
+[PHP Docker Skeleton](https://github.com/kkamara/php-docker-skeleton)
+
+[Python Docker Skeleton](https://github.com/kkamara/python-docker-skeleton).
 
 ## Contributing
 Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
--- src/MainClass.php
+++ src/MainClass.php
@@ -4,36 +4,44 @@ namespace App;
 
 require_once(__DIR__.'/../vendor/autoload.php');
 
-use GuzzleHttp\Client;
-use GuzzleHttp\Exception\RequestException;
 use stdClass;
 
 class MainClass extends stdClass
 {
-    protected $http = null;
+    protected string $file = __DIR__ . "/../tmp/anime-list.txt";
 
-    public function __construct() {
-        $this->http = new Client();
-    }
+    protected string $delimiter = "- ";
 
-    public function handle() {
-        $args = getopt('f:b:', ['foo::', 'bar::',]);
+    protected array $animeList = [
+        "Akira (1988) (dubbed)",
+        "Berserk (1997) (dubbed)",
+        "Bersek Movies (2012 - 2013) (dubbed)",
+        "Pokémon: The First Movie Mewtwo Strikes Back (2000)",
+        "Monster (2004) (dubbed)",
+        "Naruto (2002) (dubbed)",
+        "Death Note (2006) (dubbed)",
+        "Death Note Light Up The New World (2016) (only subtitles, no dubbed exists)",
+        "Death Note: The Last Name (2006) (dubbed)",
+        "Claymore (2007) (dubbed)",
+        "Highschool of the dead (2010) (dubbed)",
+        "Phantom: Requiem for the Phantom (2009) (dubbed)",
+    ];
 
-        var_dump($args);
-        
-        try {
-            $this->http->get(
-                'https://downloads.slack-edge.com/linux_releases/slack-desktop-4.16.0-amd64.deb',
-                ['sink' => __DIR__.'/../tmp/slack.deb'],
-            );
-
-            echo "success".PHP_EOL;
-        } catch (RequestException $e) {
-            echo print_r($e->getMessage(), true);
-            echo print_r($e->getResponse()->getStatusCode(), true);
-            echo print_r($e->getResponse()->getBody(), true);
+    public function run(): void {
+        sort($this->animeList);
+        file_put_contents($this->file, "");
+        $fileResource = fopen($this->file, "w+");
+        for($i = 0; $i < sizeof($this->animeList); $i++) {
+            $listItem = $this->animeList[$i];
+            echo $listItem . PHP_EOL;
+            if ($i !== sizeof($this->animeList) - 1) {
+                fwrite($fileResource, $this->delimiter . $listItem . PHP_EOL);
+            } else {
+                fwrite($fileResource, $this->delimiter . $listItem . "." . PHP_EOL);
+            }
         }
+        fclose($fileResource);
     }
 }
 
-(new MainClass())->handle();
+(new MainClass())->run();
